1a73436dfb8453d57213d6fb7744e20da6d33b90
Gerson Ramírez (Gerco) <93165594+Gxrco@users.noreply.github.com>
2023-09-19 17:31:30 -0600

    Implementacion de suma por usuario y verificación de llegada al límite

--- caloriesBurned.cpp
+++ caloriesBurned.cpp
@@ -6,8 +6,14 @@
 using namespace std;
 
 int *listaID; //Lista de ID de estudiantes
+int *listaCalories; //Lista de calorias por estudiante correspondiente de manera paralela
+bool *notificado; // Lista para rastrear si ya hemos notificado sobre un estudiante
+
 
 int generarID() {
+    /**
+     * Funcion para generar de manera aletoria cada uno de los IDs para los diferentes usuarios.
+    */
     std::string id;
     for (int i = 0; i < 5; ++i) {
         int numeroAleatorio = rand() % 10; 
@@ -26,12 +32,42 @@ int generarID() {
     }
 }
 
+void sumaPorUsuario() {
+    /**
+     * Se realizar la suma de las calorias por posicion, dado que ambas son listas se usan cada una con su
+     * posicion de 0 a 23. Esto se realiza de manera paralela
+    */
+    //Necesaria paralelización <----------------
+    for (int i = 0; i < 24; ++i) {
+        if (rand() % 100 < 60) {  //Factor de cambio para mejorar la diferencia de datos
+            int caloriasQuemadas = rand() % 20 + 1; // Quema de calorías entre 1 y 20
+            listaCalories[i] += caloriasQuemadas;
+        }
+    }
+}
+
+bool verificador(int index) {
+    /**
+     * Notifica al usuario que ID ya ha llegado a las 500 calorias o mas
+    */
+    if (listaCalories[index] >= 500 && !notificado[index]) {
+        //Valor "Critical" para utilizar unicamente un hilo por notificacion <------------------------
+        {
+            cout << "El estudiante con ID: " << listaID[index] << " ha quemado 500 calorias o mas." << endl;
+        }
+        notificado[index] = true; 
+        return true;
+    }
+    return false;
+}
+
 
 int main() {
-
     srand(static_cast<unsigned>(time(nullptr)));
 
+    notificado = new bool[24](); 
     listaID = new int[24];
+    listaCalories = new int[24](); 
 
     for (int i = 0; i < 24; ++i) {
         int idConvertido = generarID();
@@ -40,12 +76,16 @@ int main() {
         }
     }
 
-    std::cout << "ID de estudiantes:" << std::endl;
-    for (int i = 0; i < 24; ++i) {
-            cout << (i + 1) << ". " << listaID[i] << endl;
+    // Simulación de quema de calorías. Implementacion
+    for (int tiempo = 0; tiempo < 50; ++tiempo) { 
+        sumaPorUsuario();
+        for (int i = 0; i < 24; ++i) {
+            verificador(i);
+        }
     }
 
     delete[] listaID;
+    delete[] listaCalories;
 
     return 0;
 }
