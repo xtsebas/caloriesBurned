1021b71bb4e9d97c99967cfe37ffb35d4bd91940
XavierLopez25 <lop22716@uvg.edu.gt>
2023-09-19 18:55:04 -0600

    Se agregaron secciones de parallel for, critical y reduction en sumaPorUsuario, verificador y main, respectivamente. Se arregló un bug de rand() con la paralelización, se reemplazó por el Mersenne Twister Generator, números random, seguros para hilos paralelizados.

--- caloriesBurned.cpp
+++ caloriesBurned.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <string>
 #include <omp.h>
+#include <random>
 using namespace std;
 
 int *listaID; //Lista de ID de estudiantes
@@ -33,25 +34,44 @@ int generarID() {
 }
 
 void sumaPorUsuario() {
-    /**
-     * Se realizar la suma de las calorias por posicion, dado que ambas son listas se usan cada una con su
-     * posicion de 0 a 23. Esto se realiza de manera paralela
-    */
-    //Necesaria paralelización <----------------
+    // Directiva OpenMP para ejecutar el bucle for en paralelo con multiples hilos.
+    #pragma omp parallel for
     for (int i = 0; i < 24; ++i) {
-        if (rand() % 100 < 60) {  //Factor de cambio para mejorar la diferencia de datos
-            int caloriasQuemadas = rand() % 20 + 1; // Quema de calorías entre 1 y 20
+
+        // Genera una semilla unica basada en el tiempo actual y el numero de hilo.
+        unsigned seed = static_cast<unsigned>(time(nullptr)) + omp_get_thread_num();
+        
+        // Inicializa el generador de numeros aleatorios Mersenne Twister con la semilla generada.
+        std::mt19937 generator(seed);
+        
+        // Define una distribuciun uniforme que generaru numeros entre 1 y 25.
+        std::uniform_int_distribution<int> dist(1, 25);
+
+        if (dist(generator) < 60) {
+            
+            // Define una nueva distribución uniforme para generar calorías quemadas entre 1 y 25.
+            std::uniform_int_distribution<int> distCal(1, 25);
+            
+            // Genera un número aleatorio que representa las calorías quemadas en esta iteración.
+            int caloriasQuemadas = distCal(generator);
+            
+            // Suma las calorías quemadas al total de calorías del estudiante.
             listaCalories[i] += caloriasQuemadas;
+            
         }
     }
 }
+
+
 
 bool verificador(int index) {
     /**
      * Notifica al usuario que ID ya ha llegado a las 500 calorias o mas
     */
     if (listaCalories[index] >= 500 && !notificado[index]) {
-        //Valor "Critical" para utilizar unicamente un hilo por notificacion <------------------------
+
+        //Valor "Critical" para utilizar unicamente un hilo por notificacion
+        #pragma omp critical
         {
             cout << "El estudiante con ID: " << listaID[index] << " ha quemado 500 calorias o mas." << endl;
         }
@@ -84,8 +104,23 @@ int main() {
         }
     }
 
+    int totalCalorias = 0; //Variable para almacenar la suma total de calorías quemadas por todos los estudiantes 
+    #pragma omp parallel for reduction(+:totalCalorias) //Crear una copia privada de "totalCalorias" para cada hilo.
+    for(int i = 0; i < 24; ++i){
+        totalCalorias += listaCalories[i];
+    }
+
+    //Imprimir el total de calorias quemadas por la clase
+    cout << "Total de calorias quemadas por la clase: " << totalCalorias << endl;
+
+    //Imprimir el total de calorias quemadas por la clase por estudiante
+    for(int i = 0; i < 24; ++i){
+        cout << "Estudiante con ID: " << listaID[i] << " quemo " << listaCalories[i] << " calorias." << endl;
+    }
+
     delete[] listaID;
     delete[] listaCalories;
+    delete[] notificado;
 
     return 0;
 }
