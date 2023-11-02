f390ab5c26edf78beae12601649d6e350416e887
Sebastian <thiagohugo2014@gmail.com>
2023-11-02 07:21:47 -0600

    actualizacion de codigo

--- caloriesBurned.cpp
+++ caloriesBurned.cpp
@@ -1,93 +1,111 @@
 #include <iostream>
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>
 #include <string>
-#include <omp.h>
+#include <pthread.h>
 #include <random>
+#include <vector>
+#include <mutex>
+
 using namespace std;
 
-int *listaID; //Lista de ID de estudiantes
-int *listaCalories; //Lista de calorias por estudiante correspondiente de manera paralela
+int *listaID; // Lista de ID de estudiantes
+int *listaCalories; // Lista de calorías por estudiante correspondiente de manera paralela
 bool *notificado; // Lista para rastrear si ya hemos notificado sobre un estudiante
+int numThreads = 4; // Número de hilos
 
+// Mutex para garantizar acceso seguro a los datos compartidos
+std::mutex mutex;
+
+// Estructura para pasar datos a cada hilo
+struct ThreadData {
+    int id;
+};
 
 int generarID() {
     /**
-     * Funcion para generar de manera aletoria cada uno de los IDs para los diferentes usuarios.
-    */
+     * Función para generar de manera aleatoria cada uno de los IDs para los diferentes usuarios.
+     */
     std::string id;
     for (int i = 0; i < 5; ++i) {
-        int numeroAleatorio = rand() % 10; 
-        id += std::to_string(numeroAleatorio); 
+        int numeroAleatorio = rand() % 10;
+        id += std::to_string(numeroAleatorio);
     }
-    
+
     try {
         int idEntero = std::stoi(id);
         return idEntero;
     } catch (const std::invalid_argument& e) {
         std::cerr << "Error: No se pudo convertir la cadena a entero." << std::endl;
-        return 0;  
+        return 0;
     } catch (const std::out_of_range& e) {
         std::cerr << "Error: El valor está fuera del rango de representación de un entero." << std::endl;
-        return 0; 
+        return 0;
     }
 }
 
-void sumaPorUsuario() {
-    // Directiva OpenMP para ejecutar el bucle for en paralelo con multiples hilos.
-    #pragma omp parallel for
-    for (int i = 0; i < 24; ++i) {
+void sumaPorUsuario(int id) {
+    // Genera una semilla única basada en el tiempo actual y el ID del hilo.
+    unsigned seed = static_cast<unsigned>(time(nullptr)) + id;
 
-        // Genera una semilla unica basada en el tiempo actual y el numero de hilo.
-        unsigned seed = static_cast<unsigned>(time(nullptr)) + omp_get_thread_num();
-        
-        // Inicializa el generador de numeros aleatorios Mersenne Twister con la semilla generada.
-        std::mt19937 generator(seed);
-        
-        // Define una distribuciun uniforme que generaru numeros entre 1 y 25.
-        std::uniform_int_distribution<int> dist(1, 25);
+    // Inicializa el generador de números aleatorios Mersenne Twister con la semilla generada.
+    std::mt19937 generator(seed);
 
+    // Define una distribución uniforme que generará números entre 1 y 25.
+    std::uniform_int_distribution<int> dist(1, 25);
+
+    for (int tiempo = 0; tiempo < 50; ++tiempo) {
         if (dist(generator) < 60) {
-            
             // Define una nueva distribución uniforme para generar calorías quemadas entre 1 y 25.
             std::uniform_int_distribution<int> distCal(1, 25);
-            
+
             // Genera un número aleatorio que representa las calorías quemadas en esta iteración.
             int caloriasQuemadas = distCal(generator);
-            
-            // Suma las calorías quemadas al total de calorías del estudiante.
-            listaCalories[i] += caloriasQuemadas;
-            
+
+            // Bloquea el acceso al arreglo de calorías utilizando un mutex
+            mutex.lock();
+            listaCalories[id] += caloriasQuemadas;
+            mutex.unlock();
         }
     }
 }
-
-
 
 bool verificador(int index) {
     /**
-     * Notifica al usuario que ID ya ha llegado a las 500 calorias o mas
-    */
+     * Notifica al usuario que ID ya ha llegado a las 500 calorías o más
+     */
     if (listaCalories[index] >= 500 && !notificado[index]) {
 
-        //Valor "Critical" para utilizar unicamente un hilo por notificacion
-        #pragma omp critical
-        {
-            cout << "El estudiante con ID: " << listaID[index] << " ha quemado 500 calorias o mas." << endl;
-        }
-        notificado[index] = true; 
+        // Bloquea el acceso al arreglo de notificados utilizando un mutex
+        mutex.lock();
+        cout << "El estudiante con ID: " << listaID[index] << " ha quemado 500 calorías o más." << endl;
+        mutex.unlock();
+        notificado[index] = true;
         return true;
     }
     return false;
 }
 
+void *threadFunction(void *data) {
+    ThreadData *threadData = (ThreadData *)data;
+    int id = threadData->id;
+
+    sumaPorUsuario(id);
+    verificador(id);
+
+    pthread_exit(NULL);
+}
 
 int main() {
-    srand(static_cast<unsigned>(time(nullptr)));
+    srand(static_cast<unsigned>(time(nullptr));
 
-    notificado = new bool[24](); 
+    notificado = new bool[24]();
     listaID = new int[24];
-    listaCalories = new int[24](); 
+    listaCalories = new int[24]();
+
+    // Crear hilos
+    pthread_t threads[numThreads];
+    std::vector<ThreadData> threadData(numThreads);
 
     for (int i = 0; i < 24; ++i) {
         int idConvertido = generarID();
@@ -96,26 +114,28 @@ int main() {
         }
     }
 
-    // Simulación de quema de calorías. Implementacion
-    for (int tiempo = 0; tiempo < 50; ++tiempo) { 
-        sumaPorUsuario();
-        for (int i = 0; i < 24; ++i) {
-            verificador(i);
-        }
+    // Simulación de quema de calorías
+    for (int i = 0; i < numThreads; ++i) {
+        threadData[i].id = i;
+        pthread_create(&threads[i], NULL, threadFunction, &threadData[i]);
     }
 
-    int totalCalorias = 0; //Variable para almacenar la suma total de calorías quemadas por todos los estudiantes 
-    #pragma omp parallel for reduction(+:totalCalorias) //Crear una copia privada de "totalCalorias" para cada hilo.
-    for(int i = 0; i < 24; ++i){
+    // Esperar a que todos los hilos terminen
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    int totalCalorias = 0; // Variable para almacenar la suma total de calorías quemadas por todos los estudiantes
+    for (int i = 0; i < 24; ++i) {
         totalCalorias += listaCalories[i];
     }
 
-    //Imprimir el total de calorias quemadas por la clase
-    cout << "Total de calorias quemadas por la clase: " << totalCalorias << endl;
+    // Imprimir el total de calorías quemadas por la clase
+    cout << "Total de calorías quemadas por la clase: " << totalCalorias << endl;
 
-    //Imprimir el total de calorias quemadas por la clase por estudiante
-    for(int i = 0; i < 24; ++i){
-        cout << "Estudiante con ID: " << listaID[i] << " quemo " << listaCalories[i] << " calorias." << endl;
+    // Imprimir el total de calorías quemadas por la clase por estudiante
+    for (int i = 0; i < 24; ++i) {
+        cout << "Estudiante con ID: " << listaID[i] << " quemó " << listaCalories[i] << " calorías." << endl;
     }
 
     delete[] listaID;
